20428262331f8d049496740a0f95646a6b6a72d0
Hesham <Heshamoov90@gmail.com>
2020-01-01 22:02:03 +0400

    Firt Push

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(Hesham());
+
+class Hesham extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(appBar: AppBar(title: Text('Hesham'),),),);
+  }
+}

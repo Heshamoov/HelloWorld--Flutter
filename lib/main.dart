131522ac0a15966e6d6bb6176fb6ab2c901e93f5
Hesham <Heshamoov90@gmail.com>
2020-01-01 23:14:54 +0400

    Scaffold (Anonymous Functions)

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,38 @@ import 'package:flutter/material.dart';
 void main() => runApp(Hesham());
 
 class Hesham extends StatelessWidget {
+  void printText(){
+    print('Hello');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold(appBar: AppBar(title: Text('Hesham'),),),);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('HESHAM'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text('Questions'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: printText,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                // ...
+                print('Answer 3');
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
